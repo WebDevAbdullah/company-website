9c12b0de3a60502c987bf30c9db889193542c39e
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-07-06 14:00:42 +0400

    Navbar and Main Carousel are done

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.min";
+import "../css/style.css";
+import "../sass/style.scss";
+
+// import "jquery/dist/jquery.min.js";
